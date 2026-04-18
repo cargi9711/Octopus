662f400229a73ae132aca3dd614b82b06b199a30
Bletokz <39291212+cargi9711@users.noreply.github.com>
2026-04-18 18:26:29 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from octopus import Octopus
+from ocean_habitat import OceanHabitat
+
+octo = Octopus("Inky")
+habitat = OceanHabitat(200, 10)
+
+print(octo)
+print(octo.ink())
+print(habitat.describe())
